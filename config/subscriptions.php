8f1d500ff13cd258bdf9310ebfb8311b1457f4d6
gustavo.mfernandes <gustavo.mfernandes@fiocruz.br>
2025-12-15 20:03:59 -0300

    alterando valores

--- config/subscriptions.php
+++ config/subscriptions.php
@@ -4,19 +4,19 @@ return [
     'plans' => [
         'mensal' => [
             'name' => 'Plano Mensal',
-            'price' => 59.99,
+            'price' => 30.00,
             'months' => 1,
             'external_reference' => 'BarberAppMensal'
         ],
         'trimestral' => [
             'name' => 'Plano Trimestral',
-            'price' => 159.99,
+            'price' => 90.00,
             'months' => 3,
             'external_reference' => 'BarberAppTrimestral',
         ],
         'anual' => [
             'name' => 'Plano Anual',
-            'price' => 659.99,
+            'price' => 360.00,
             'months' => 12,
             'external_reference' => 'BarberAppAnual',
         ],
